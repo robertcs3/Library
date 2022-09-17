4e28c48ae985264c874447662c35aed60aafe75e
Robert Stebbins <48532176+robertstebbins@users.noreply.github.com>
2022-09-17 02:37:43 -0500

    Updated remove functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myLibrary = [
-   
+
 ]
 
 
@@ -19,49 +19,67 @@ function addBookToLibrary(title, author, pages, status) {
     book.pages = pages;
     book.status = status;
     myLibrary.push(book);
-    
+
 }
 
 let existingBooks = [];
 
 /* Display books in catalog */
-function displayCatalog () {
-   
-
+function displayCatalog() {
 
     myLibrary.forEach(book => {
-        if (!existingBooks.includes(book.title)){
-        let card = document.createElement('div');
-        card.setAttribute('class', 'card');
-        card.setAttribute('id', myLibrary.indexOf(book));
-        let title = document.createElement('p');
-        let author = document.createElement('p');
-        let pages = document.createElement('p');
-        let status = document.createElement('button');
-        let button = document.createElement('button');
-        button.setAttribute('class', myLibrary.indexOf(book));
-        /* Add card class to catalog */
-        catalog.appendChild(card);
+        /* Add book to catalog if it doesn't exist already */
+        if (!existingBooks.includes(book.title)) {
+            let card = document.createElement('div');
+            card.setAttribute('class', 'card');
+            card.setAttribute('id', myLibrary.indexOf(book));
+            let title = document.createElement('p');
+            let author = document.createElement('p');
+            let pages = document.createElement('p');
+            let status = document.createElement('button');
+            let button = document.createElement('button');
+            button.setAttribute('class', myLibrary.indexOf(book));
+            /* Add card class to catalog */
+            catalog.appendChild(card);
 
-        /* Add attributes of book to the card */
-        card.appendChild(title);
-        card.appendChild(author);
-        card.appendChild(pages);
-        card.appendChild(status);
-        card.appendChild(button);
+            /* Add attributes of book to the card */
+            card.appendChild(title);
+            card.appendChild(author);
+            card.appendChild(pages);
+            card.appendChild(status);
+            card.appendChild(button);
 
-        title.textContent = "Title: " + book.title;
-        author.textContent = "Author: " + book.author;
-        pages.textContent = "Pages: " + book.pages;
-        status.textContent = book.status;
-        button.textContent = "Remove";
+            title.textContent = "Title: " + book.title;
+            author.textContent = "Author: " + book.author;
+            pages.textContent = "Pages: " + book.pages;
+            status.textContent = book.status;
+            button.textContent = "Remove";
 
-        existingBooks.push(book.title);
+            existingBooks.push(book.title);
+
+            remove();
+
+
 
         }
-        
+
     })
-    
+
+}
+
+function remove() {
+    let remove = document.querySelectorAll('.card > button:last-child');
+    for (const element of remove) {
+        element.addEventListener('click', () => {
+
+            myLibrary.forEach(book => {
+                if (myLibrary.indexOf(book) == element.className) {
+                    element.parentElement.remove(element.parentElement.children)
+                }
+            })
+        });
+    }
+
 }
 let catalog = document.getElementById('catalog-container');
 
@@ -87,41 +105,42 @@ let pagesInput = document.getElementById('page-numbers');
 
 let statusInput = document.getElementById('status-input');
 
+
+/* Add a new book */
 submit.addEventListener('click', () => {
     if (!titleInput.value == '' && !authorInput.value == ''
-     && !pagesInput.value == '') {
+        && !pagesInput.value == '') {
         addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value,
             statusInput.value);
 
         titleInput.value = '';
         authorInput.value = '';
         pagesInput.value = '';
-        statusInput.value = '';
+        
 
         displayCatalog();
 
-     }
-   
+        form.style.display = 'none';
 
-       
-    
-    console.log(myLibrary);
+    }
+
 })
 
 
 
 
 
+/* Show or hide form  when button is clicked */
 addNew.addEventListener('click', () => {
-   
-    /* Show or hide form  when button is clicked */
+
+
     if (form.style.display === 'none') {
-      
+
         form.style.display = 'block';
-      } else {
-       
+    } else {
+
         form.style.display = 'none';
-      }
+    }
 })
 
 
